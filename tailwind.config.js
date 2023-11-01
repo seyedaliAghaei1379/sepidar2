192e425a2eff93ddb0db8c2cb1c6304d310555d7
ali <seyedali1379aghaei@gmail.com>
2023-11-01 13:12:02 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend : {
+      colors : {
+        primary : {
+          100 : "#e20e65",
+          300 : "#7EBFFF",
+          500 : "#2987E5",
+          900 : "#081725",
+        },
+        gray : {
+          100 : "#FAFAFA",
+          200 : "#F3F3F3",
+          500 : "#E4E4E4",
+          700 : "#A8ABB4",
+        },
+        secondary : {
+          100 : "#EDFFF4",
+          500 : "#73D697",
+        }
+      }
+    }
+  },
+  plugins: [],
+}
